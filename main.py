d97fde1f546b88a30fdf07ba30a2518d6ff31077
Rohith Gilla <gillarohith1@gmail.com>
2018-06-04 14:45:43 +0530

    PEP 8 validations

--- main.py
+++ main.py
@@ -1,53 +1,52 @@
-import os #python module for mimicking actions done by native operating system
-from pushbullet import Pushbullet #install this module using "sudo pip install pushbullet.py"
+import os  # Python module for mimicking actions done by native operating system
+from pushbullet import Pushbullet  # Install this module using "sudo pip install pushbullet.py"
 
 
-class PB:                           #this class contains all the functions required
-    def __init__(self, api_token):  #constructor , get your API token from https://www.pushbullet.com/#settings
+class PB:                           # This class contains all the functions required
+    def __init__(self, api_token):  # Constructor , get your API token from https://www.pushbullet.com/#settings
         self.api_token = api_token
 
-    def authorise(self):            #authorisation from pushbucket,we wil be able to send and receive only if authorised ,and this authorisation is required for sending /receiving messages
-        return Pushbullet(self.api_token)  #returns auth keys
+    def authorise(self):            # This authorisation is required for sending /receiving messages
+        return Pushbullet(self.api_token)  # Returns auth keys
 
-    def send(self, title, body):     #sending a message to pushbullet (can be read from any device)
+    def send(self, title, body):     # Sending a message to pushbullet (can be read from any device)
         try :
-            self.authorise().push_note(title, body) #sending message
-            return 0                                #0 indicates sent succefully
+            self.authorise().push_note(title, body)  # Sending message
+            return 0                                # 0 indicates sent successfully
         except :
             print("unable to send the output to pushhbullet") 
-            return -1                               #-1 indicates sent unsuccesfully
+            return -1                               # -1 indicates sent unsuccessfully
 
-    def receive_latest(self):                       #for retreiving the latest message
+    def receive_latest(self):                       # For retriving the latest message
         tempPushes = self.authorise().get_pushes()
         for i in tempPushes:
             if i.get('type') == "note":
                 if i.get('body').split(" ")[0] == "/cmd":
                     return i.get('body')
 
-    def get_cmd(self):                              #parsing the latest message for retrieving the command
+    def get_cmd(self):                              # Parsing the latest message for retrieving the command
         msg = self.receive_latest()
         cmd = msg.split(" ")
         cmd.pop(0)
         return " ".join(cmd)
 
-    def execute_cmd(self):                          #for executing the command retrieved
-        return os.system(self.get_cmd() + " > templog.txt") #executes the command and writes the output into the file templog.txt
+    def execute_cmd(self):                          # For executing the command retrieved
+        return os.system(self.get_cmd() + " > templog.txt")  # Executes  and writes the output into the file templog.txt
 
-    def send_output(self):       #sends the output from the command executed to pushbullet using the send function 
+    def send_output(self):       # Sends the output from the command executed to pushbullet using the send function
         k=self.execute_cmd()
-        if k==0:
+        if k == 0:
             with open("dude.txt", 'r') as op:
                 final_string = op.readlines()
                 final_string=''.join(final_string)
                 self.send("Output of "+self.get_cmd()+" :", final_string)
-        else : 
+        else:
             self.send("Output of "+self.get_cmd()+" :", "command not found / unsuccesful exit")        
 
 
-
-if __name__ == '__main__':
-    f=PB("your api token") # get your API token from https://www.pushbullet.com/#settings
-    f.send_output()
+if __name__ == "__main__":
+    First = PB("Your Push bullet api key")  # API token from https://www.pushbullet.com/#settings
+    First.send_user()
 
 
 
